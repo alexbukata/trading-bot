10c1c78abcd4c7d52e84a1800e8e6c432540dd1a
desio05 <gcinbax@gmail.com>
2021-05-11 19:03:55 +0300

    init

---
+++ src/main/main.py
@@ -0,0 +1,9 @@
+
+if __name__ == "__main__":
+    pass
+    # schedule price retrieving
+    # on event:
+    #   1. get price,
+    #   2. calculate model input
+    #   3. pass into model, get result
+    #   4. sell/buy
